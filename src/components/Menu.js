246d79731c43c2046ee5c966d12d83fe9e37a049
Tri Phan <tripp@ames.edu.vn>
2022-10-11 08:13:17 +0700

    Updated

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Recipe from './Recipe'
+import StarRating from './StarRating'
 
-function Menu({ recipes }) {
-	return (
-		<article>
-			<header>
-				<h1>Delicous Recipes</h1>
-			</header>
-			<div className='recipes'>
-				{recipes.map((recipe, i) => (
-					<Recipe key={i} {...recipe} />
-				))}
-			</div>
-		</article>
-	)
+function Menu() {
+	return <StarRating />
 }
+// function Menu({ recipes }) {
+// 	return (
+// 		<article>
+// 			<header>
+// 				<h1>Delicous Recipes</h1>
+// 			</header>
+// 			<div className='recipes'>
+// 				{recipes.map((recipe, i) => (
+// 					<Recipe key={i} {...recipe} />
+// 				))}
+// 			</div>
+// 		</article>
+// 	)
+// }
 
 export default Menu
---
+++ src/components/StarRating.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Star from './Star'
+
+const createArray = (length) => [...Array(length)]
+
+function StarRating({ totalStars = 5 }) {
+	const [selectedStars, setSelectedStars] = useState(0)
+
+	return (
+		<>
+			{createArray(totalStars).map((n, i) => (
+				<Star
+					key={i}
+					selected={selectedStars > i}
+					onSelect={() => setSelectedStars(i + 1)}
+				/>
+			))}
+			<p>
+				{selectedStars} of {totalStars} stars.
+			</p>
+		</>
+	)
+}
+
+export default StarRating
